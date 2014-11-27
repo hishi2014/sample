88145fff724497e94ef38d40612c54f78cc6274f
hishi2014 <hisahiro_shirai2014@yahoo.co.jp>
2014-11-28 08:05:45 +0900

    fourteenth commit

--- core/Db.php
+++ core/Db.php
@@ -6,7 +6,7 @@ Class Db{
 function __construct(){
 try {
     $options = array();
-    $this->dbh = new PDO('pgsql:host=107.20.159.155:5432;dbname=dahrcqpca41iek',"kyffrcjeajphaz", "Show");
+    $this->dbh = new PDO('pgsql:host=107.20.159.155;port=5432;dbname=dahrcqpca41iek',"kyffrcjeajphaz", "Show");
     } catch (PDOException $e) {
     print "error!: " . $e->getMessage() . "<br/>";
     die();
