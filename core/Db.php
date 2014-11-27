e10612c2cb25c64b5b468aec444788c3d8bb365e
hishi2014 <hisahiro_shirai2014@yahoo.co.jp>
2014-11-27 16:39:15 +0900

    first commit

--- core/Db.php
+++ core/Db.php
@@ -8,7 +8,7 @@ try {
     $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
     );
-    $this->dbh = new PDO('mysql:host=localhost;dbname=test3;unix_socket=/tmp/mysql.sock', "root", "perorin5",$options);
+    $this->dbh = new PDO(,$options);
     } catch (PDOException $e) {
     print "error!: " . $e->getMessage() . "<br/>";
     die();
